8c850bd03bedc2e0eeb548014cf42b5ade8fc99b
kkito <k.kito0726@gmail.com>
2025-02-21 23:22:39 +0900

    テスト追加

--- reagent-order-kotlin-backend/reagent_order/src/test/kotlin/kkito/reagent_order/app_user/UpdateAppUserTest.kt
+++ reagent-order-kotlin-backend/reagent_order/src/test/kotlin/kkito/reagent_order/app_user/UpdateAppUserTest.kt
@@ -5,6 +5,7 @@ import kkito.reagent_order.TestSupport
 import kkito.reagent_order.app_user.value.UpdateAppUserRequest
 import kkito.reagent_order.app_user.value.Role
 import kkito.reagent_order.error.ErrorCode
+import kkito.reagent_order.lib.fixture.AppUserFixture
 import kkito.reagent_order.test_data.TestDataAppUser
 import org.assertj.db.api.Assertions
 import org.json.JSONObject
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -29,6 +31,7 @@ class UpdateAppUserTest(
     @Autowired private val mockMvc: MockMvc,
     @Autowired private val objectMapper: ObjectMapper,
     @Autowired private val testDataAppUser: TestDataAppUser,
+    @Autowired private val passwordEncoder: PasswordEncoder,
 ) : TestSupport() {
     companion object {
         private val TABLE_NAMES = listOf("app_user")
@@ -95,6 +98,53 @@ class UpdateAppUserTest(
             .value("created_at").isNotNull()
             .value("deleted_at").isNull()
             .value("role").isEqualTo("USER")
+    }
+
+    @Test
+    fun システム管理者が一般ユーザーの権限を管理者に変更できる() {
+        val systemUser = AppUserFixture(passwordEncoder = passwordEncoder)
+        systemUser.insert(dslContext)
+        val systemUserJwt = testDataAppUser.login(systemUser.email, systemUser.password)
+
+        val request = UpdateAppUserRequest(
+            createdUserResponse.getString("appUserName"),
+            createdUserResponse.getString("email"),
+            "Test_pass_12345678",
+            Role.ADMIN.value
+        )
+
+        val changes = createChanges(TABLE_NAMES).setStartPointNow()
+        val resultActions = mockMvc.perform(
+            put("/app_user/${createdUserResponse.getString("id")}")
+                .header("Authorization", "Bearer $systemUserJwt")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isOk)
+        changes.setEndPointNow()
+
+        // レスポンスのアサート
+        val responseBody = createResponseBodyJson(resultActions)
+        assertNotNull(responseBody.getString("id"))
+        assertEquals(responseBody.getString("appUserName"), request.appUserName)
+        assertEquals(responseBody.getString("email"), request.email)
+        assertEquals(responseBody.getString("role"), Role.ADMIN.value)
+
+        Assertions.assertThat(changes)
+            .ofModificationOnTable("app_user")
+            .hasNumberOfChanges(1)
+            .ofDeletionOnTable("app_user")
+            .hasNumberOfChanges(0)
+            .ofCreationOnTable("app_user")
+            .hasNumberOfChanges(0)
+            .changeOfModification()
+            .rowAtEndPoint()
+            .value("id").isNotNull()
+            .value("app_user_name").isEqualTo(request.appUserName)
+            .value("email").isEqualTo(request.email)
+            .value("password").isNotNull
+            .value("created_at").isNotNull()
+            .value("deleted_at").isNull()
+            .value("role").isEqualTo("ADMIN")
     }
 
     @Test
@@ -249,5 +299,26 @@ class UpdateAppUserTest(
 
         assertEquals(ErrorCode.E0010.code, responseBody.getString("errorCode"))
         assertEquals(ErrorCode.E0010.message, responseBody.getString("message"))
+    }
+
+    @Test
+    fun 一般ユーザーが権限を変更しようとする時_E0009エラーになる() {
+        val request = UpdateAppUserRequest(
+            "てすと 太郎",
+            "second_user@test.gmail.com",
+            "Test_pass_12345678",
+            "ADMIN"
+        )
+
+        val resultActions = mockMvc.perform(
+            put("/app_user/${createdUserResponse.getString("id")}")
+                .header("Authorization", "Bearer $jwtToken")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isForbidden)
+        // レスポンスのアサート
+        val responseBody = createResponseBodyJson(resultActions)
+        assertEquals(ErrorCode.E0009.code, responseBody.getString("errorCode"))
+        assertEquals(ErrorCode.E0009.message, responseBody.getString("message"))
     }
 }
---
+++ reagent-order-kotlin-backend/reagent_order/src/test/kotlin/kkito/reagent_order/lib/fixture/AppUserFixture.kt
@@ -0,0 +1,46 @@
+package kkito.reagent_order.lib.fixture
+
+import com.generate.jooq.Tables
+import org.jooq.DSLContext
+import org.springframework.security.crypto.password.PasswordEncoder
+import java.time.LocalDateTime
+import java.util.UUID
+
+class AppUserFixture(
+    val id: UUID = UUID.randomUUID(),
+    val appUserName: String = "テストユーザー",
+    val email: String = "test@fixture.com",
+    val password: String = "Fixture12345678",
+    private val createdAt: LocalDateTime = LocalDateTime.now(),
+    private val deletedAt: LocalDateTime? = null,
+    private val role: String = "SYSTEM",
+    passwordEncoder: PasswordEncoder // 依存性注入
+) {
+    private val encodedPassword: String = passwordEncoder.encode(password) // エンコード済みパスワード
+
+    fun insert(dslContext: DSLContext): Int {
+        return dslContext.insertInto(Tables.APP_USER)
+            .columns(
+                Tables.APP_USER.ID,
+                Tables.APP_USER.APP_USER_NAME,
+                Tables.APP_USER.EMAIL,
+                Tables.APP_USER.PASSWORD,
+                Tables.APP_USER.CREATED_AT,
+                Tables.APP_USER.DELETED_AT,
+                Tables.APP_USER.ROLE
+            ).values(
+                id.toString(),
+                appUserName,
+                email,
+                encodedPassword,
+                createdAt,
+                deletedAt,
+                role
+            ).execute()
+    }
+}
+
+// 拡張関数を追加
+fun DSLContext.insertAppUser(fixture: AppUserFixture): Int {
+    return fixture.insert(this)
+}
